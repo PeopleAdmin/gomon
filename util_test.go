eab442225b366e69403a9a7c629efe4a72153cea
Carl Brasic <carl.brasic@peopleadmin.com>
2013-10-17 11:36:33 -0500

    Add spec for DeltaSinceLastChange

--- util_test.go
+++ util_test.go
@@ -10,9 +10,9 @@ var _ = Describe("Util", func() {
 	Describe("calculateStats", func() {
 		var slice []float64
 
-		Context("called with a slice of all zeros", func(){
-			It("should return the appropriate stats", func(){
-				slice = []float64{0.0}
+		Context("called with a slice of all zeros", func() {
+			slice = []float64{0.0}
+			It("should return the appropriate stats", func() {
 				allZero, min, max, sum := calculateStats(slice)
 				Expect(allZero).To(BeTrue())
 				Expect(min).To(BeZero())
@@ -21,13 +21,48 @@ var _ = Describe("Util", func() {
 			})
 		})
 
-		Context("with an empty slice", func(){
-			It("should panic", func(){
+		Context("with an empty slice", func() {
+			It("should panic", func() {
 				slice = []float64{}
-				Expect(func(){ calculateStats(slice) }).To(Panic())
+				Expect(func() { calculateStats(slice) }).To(Panic())
+			})
+		})
+
+	})
+
+	Describe("deltaSinceLastCall", func() {
+		var emitter func() float64
+		emitter = Fib()
+
+		Context("with a function that changes", func() {
+			It("reflects the changes in successive calls", func() {
+				delta := DeltaSinceLastCall(emitter)
+				Expect(delta()).To(Equal(0.0))
+				Expect(delta()).To(Equal(1.0))
+				Expect(delta()).To(Equal(1.0))
+				Expect(delta()).To(Equal(2.0))
+			})
+		})
+
+		Context("with a function that does not change", func() {
+			It("always shows zero delta", func() {
+				delta := DeltaSinceLastCall(func() float64 { return 1.0 })
+				Expect(delta()).To(Equal(0.0))
+				Expect(delta()).To(Equal(0.0))
+				Expect(delta()).To(Equal(0.0))
 			})
 		})
 
 	})
 
 })
+
+// When called successively, returns the next term in the Fibonacci sequence.
+func Fib() (generatorFunc func() float64) {
+	a, b := 0.0, 1.0
+	generatorFunc = func() float64 {
+		a, b = b, a+b
+		return a
+	}
+	return
+}
